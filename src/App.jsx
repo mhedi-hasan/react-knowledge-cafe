0fa4300fb019df047623487d23b0e46b97bc1d81
Mehedi Hasan <mhedih32@gmail.com>
2024-03-15 10:09:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-5xl text-center font-bold mt-3'>React Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
