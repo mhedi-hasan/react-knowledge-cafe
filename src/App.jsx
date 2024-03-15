5c9d3345a663240652028fdd526627bb67fb731a
Mehedi Hasan <mhedih32@gmail.com>
2024-03-15 11:03:47 +0600

    images file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 import Header from './components/Header/Header'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,14 @@
+import { data } from "autoprefixer";
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Blogs = () => {
+    const [blogs,setBlogs]=useState([])
+    useEffect(() =>{
+        fetch('blogs.json')
+        .then(res =>res.json())
+        .then(data => setBlogs(data))
+    },[])
     return (
         <div>
             
