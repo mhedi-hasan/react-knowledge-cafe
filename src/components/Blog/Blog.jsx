5d68008240a36b63969689efa1202737271a08f9
Mehedi Hasan <mhedih32@gmail.com>
2024-03-15 11:50:25 +0600

    PropTypes content added

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+
+const Blog = ({blog}) => {
+    console.log(blog)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blog;
--- src/components/BooksMarks/BooksMarks.jsx
+++ src/components/BooksMarks/BooksMarks.jsx
@@ -1,6 +1,6 @@
 const BooksMarks = () => {
     return (
-        <div className="lg:w-1/3 w-full bg-orange-500">
+        <div className="lg:w-1/3 w-full">
             <h2>BooksMarks</h2>
         </div>
     );
